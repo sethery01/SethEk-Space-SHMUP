dfa794ba983d4a8ed21080132cae4141d031e963
Ek <sgebm6@umsystem.edu>
2025-10-15 13:42:35 -0500

    Adding project files

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero S { get; private set; }  // Singleton property    // a
+
+    [Header("Inscribed")]
+    // These fields control the movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Dynamic")]
+    [Range(0, 4)]                                          // b
+    public float shieldLevel = 1;
+
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this; // Set the Singleton only if it’s null                  // c
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
+
+    void Update()
+    {
+        // Pull in information from the Input class
+        float hAxis = Input.GetAxis("Horizontal");                            // d
+        float vAxis = Input.GetAxis("Vertical");                              // d
+
+        // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += hAxis * speed * Time.deltaTime;
+        pos.y += vAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Rotate the ship to make it feel more dynamic                       // e
+        transform.rotation = Quaternion.Euler(vAxis * pitchMult, hAxis * rollMult, 0);
+    }
+
+    // void Start() {…}  // Please delete the unused Start() method
+}
